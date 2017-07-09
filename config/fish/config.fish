498154825a5e49b94c24d478d6b89495e56ed783
Kyle Prestel <kprestel@gmail.com>
2017-07-09 15:35:21 -0400

    re_search.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,4 @@
 eval (thefuck --alias | tr '\n' ';')
 . ~/z-fish/z.fish
+set fish_key_bindings fish_user_key_bindings
+set -gx PATH $PATH ~/bin
--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,25 +1,11 @@
-
-fzf_key_bindings
 function fish_user_key_bindings
-    ### fzf ###
-    bind \ct '__fzf_ctrl_t'
-    bind \cr '__fzf_ctrl_r'
-    bind \cx '__fzf_ctrl_x'
-    bind \ec '__fzf_alt_c'
-    if bind -M insert >/dev/null ^/dev/null
-        bind -M insert \ct '__fzf_ctrl_t'
-        bind -M insert \cr '__fzf_ctrl_r'
-        bind -M insert \cx '__fzf_ctrl_x'
-        bind -M insert \ec '__fzf_alt_c'
-end
-### fzf ###
-
 fish_vi_key_bindings
-bind -M insert -m dckward-char force-repaint
+bind -M insert -m default backward-char force-repaint
 
 for mode in insert default visual
     bind -M $mode \cf forward-char
    end
 
+bind \cr re_search
 
 end
