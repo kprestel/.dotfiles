4e53dd41b9afb0f36aee536b739833c588f26862
Kyle Prestel <kprestel@gmail.com>
2017-07-15 17:31:45 -0400

    add new aws-ls command.

---
+++ config/fish/functions/aws-ls.fish
@@ -0,0 +1,3 @@
+function aws-ls
+  ssh ec2-user@34.202.69.127
+end
--- config/omf/before.init.fish
+++ config/omf/before.init.fish
@@ -1,3 +1,4 @@
 set -xg Z_SCRIPT_PATH ~/z-fish/z.fish
 set -xg VIRTUALFISH_DEFAULT_PYTHON python3.6
 set -xg PROJECT_HOME ~/workspace/
+set -xg DOCKER_MACHINE_DEFAULT dev
